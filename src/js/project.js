534998a41ef183bfca87ffbc47f8d42a88cd2e46
James Sullivan <jamessullivan@jamess-air.home>
2020-07-02 09:15:37 +0100

    first commit

---
+++ src/js/project.js
@@ -0,0 +1,11 @@
+document.title = 'blah';
+
+document.querySelector(".title").textContent = "My awesome Gulpy file";
+
+const message = () => {
+    console.log("The Javascript was transpiled")
+}
+
+
+
+message();
